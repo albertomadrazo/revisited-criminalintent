fa2ad04e238aa433803fdac0dc71de9b5c872f88
unknown <it04@IT04.FESA.com>
2016-09-01 08:57:42 -0500

    Crea DatePickerFragment y un date_picker layout.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,22 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+
+public class DatePickerFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState){
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(v)
+                .setTitle(R.string.date_picker_title)// listener (null for now)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+    }
+}
